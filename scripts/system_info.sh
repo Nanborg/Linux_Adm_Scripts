a48b34da6faed7588c492ad75663ef7a53bff0e9
Nicolsan <nicolas.sanchezroca123@gmail.com>
2026-07-26 00:07:32 +0200

    Add OS information to system info script

--- scripts/system_info.sh
+++ scripts/system_info.sh
@@ -8,3 +8,8 @@ echo "Kernel: $(uname -r)"
 echo "Architecture: $(uname -m)"
 echo "Uptime: $(uptime -p)"
 echo "Date: $(date "+%Y-%m-%d %H:%M:%S")"
+
+os=name="uknown"
+if [ -r /etc/os-release ]; then
+    os_name=$(awk -F= '$1=="PRETTY_NAME" {gsub("\"", "", $2); print $2}' /etc/os-release)
+fi

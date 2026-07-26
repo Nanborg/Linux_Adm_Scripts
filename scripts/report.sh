34aec4ff7bdc0023bae49abaff459292c849aa85
Nicolsan <nicolas.sanchezroca123@gmail.com>
2026-07-26 23:05:37 +0200

    Add stdout mode to report script

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+usage() {
+    echo "Usage $0 [--stdout] [--help]"
+}
+
+output="file"
+
+case "$1" in
+    "")
+        ;;
+    --stdout)
+        output="stdout"
+        ;;
+    --help|-h)
+        usage
+        exit 0
+        ;;
+    *)
+        echo "Error: unknown option: $1" >&2
+        usage
+        exit 1
+        ;;
+esac
+
 generate_report() {
     echo "System report"
     echo "============="
@@ -22,7 +45,10 @@ generate_report() {
     who
 }
 
-
+if [ "$output" = "stdout" ]; then
+    generate_report
+    exit 0;
+fi
 
 report_dir="reports"
 

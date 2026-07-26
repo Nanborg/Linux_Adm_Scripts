343b20d58bcd2fab408d595abba3109a42c23347
Nicolsan <nicolas.sanchezroca123@gmail.com>
2026-07-26 11:36:05 +0200

    Check systemctl availability

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+if ! command -v systemctl >/dev/null 2>&1; then
+    echo "Error: systemctl is nor available on this system" >&2
+    exit 1
+fi
+
 echo "Running services"
 echo "================"
 systemctl list-units --type=service --state=running --no-pager

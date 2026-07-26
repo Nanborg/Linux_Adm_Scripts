cb90b6a64d10cb49819f67d4048c1a74287006fd
Nicolsan <nicolas.sanchezroca123@gmail.com>
2026-07-26 12:26:21 +0200

    Finalize services script

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
+usage() {
+    echo "Usage: $0 [service_name]"
+    echo
+    echo "Examples:"
+    echo " $0"
+    echo " $0 ssh"
+    echo " $0 sshd"
+}
+
+case "$1" in
+    --help|-h)
+        usage
+        exit 0
+        ;;
+esac
+
+
 
 if ! command -v systemctl >/dev/null 2>&1; then
     echo "Error: systemctl is nor available on this system" >&2
     exit 1
 fi
 
-echo "Running services"
-echo "================"
-systemctl list-units --type=service --state=running --no-pager
+service="$1"
+
+if [ -z "$service" ]; then
+    echo "Running services"
+    echo "================"
+    systemctl list-units --type=service --state=running --no-pager
+    exit 0
+fi
+
+if systemctl is-active --quiet "$service"; then
+    echo "Status: $service is active"
+    exit 0
+fi
+
+echo "Status: $service is not active or not found"
+exit 2
